5bd40fbeb0fe45c3d01208b9d74092afe05bb619
kamrul-07 <kamrul.i.akib@gmail.com>
2021-09-26 21:41:26 +0600

    solvedd

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -5,13 +5,13 @@ const Header = () => {
         
             <div className="container mt-2">
            <div className="header-style p-3 text-white rounded">
-               <h1>Motivational Speakers</h1>
+               <h1>Motivational Speakers Income</h1>
                <p>We've ranked the top  motivational speakers from around the world,based on popularity</p>
                <h2>Total income:$300 Million</h2>
            </div>
         </div>
         
-    );
+    )
 };
 
 export default Header;
